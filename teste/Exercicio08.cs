59d992dd97c2ea09310693aa2c138d013b3db43d
andre010101 <andrepessoa182@hotmail.com>
2022-09-25 19:29:55 -0300

    atividade 01 academia atos

---
+++ teste/Exercicio08.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe.atos.teste
+{
+    internal class Exercicio08
+    {
+        public static void Ex08()
+        {
+            double Celsius, Fahrenheit;
+
+            Celsius = 36;
+            Fahrenheit = (9 * Celsius + 160) / 5;
+
+            Console.WriteLine($"a temperatura convertida de Celsius para Fahrenheit é de {Fahrenheit}°F\r\n");
+        }
+    }
+}
